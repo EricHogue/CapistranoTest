4a7074c997e4c97a9e0da5d5a6f641e3250a120a
Eric Hogue <eric@erichogue.ca>
2013-02-19 15:29:41 -0500

    Added code for testing

---
+++ src/RDM/Init.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace RDM;
+
+class Init {
+}
+
---
+++ tests/RDM/InitTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RDM\Tests;
+
+use RDM\Init;
+
+class InitTest extends \PHPUnit_Framework_TestCase {
+	/** @test */
+	public function loadDependency() {
+		$init = new Init();
+	}
+}
+
